908e7236c8af42cd72ceefc9e63295fcc1269541
Pyaephyo33 <pyaephyolinn778899@gmail.com>
2023-11-15 21:50:55 +0630

    ppl

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
     <div class="max-w-7xl mx-auto">
-        <h1>Header</h1>
+        <!-- <h1>Header</h1> -->
+        <Navbar />
         <RouterView/>
     </div>
 </template>
